476783a5e4817235868b2fca2785ad6a19cea321
saipranathi <saipranathib>
2020-05-19 21:00:14 +0530

    application.properties added n resource folder to connect to Mysql database

--- src/main/java/fr/ing/interview/SpringBootCrudOperationApplication.java
+++ src/main/java/fr/ing/interview/SpringBootCrudOperationApplication.java
@@ -8,8 +8,7 @@ public class SpringBootCrudOperationApplication {
 
 	public static void main(String[] args) {
 
-		
-		
+	
 		SpringApplication.run(SpringBootCrudOperationApplication.class, args);
 
 	}
