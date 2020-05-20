eb5728d70099c251d5d806e0fced40f84489b494
saipranathi <saipranathib>
2020-05-21 01:59:56 +0530

    Adding Loggers to Transaction Service

--- src/main/java/fr/ing/interview/ServiceImpl/TransactionServiceImpl.java
+++ src/main/java/fr/ing/interview/ServiceImpl/TransactionServiceImpl.java
@@ -2,10 +2,12 @@ package fr.ing.interview.ServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.ing.interview.Controller.AccountController;
 import fr.ing.interview.DAO.AccountDAO;
 import fr.ing.interview.DAO.TransactionDAO;
 import fr.ing.interview.Exception.AccountNumberNotFoundException;
@@ -22,9 +24,12 @@ public class TransactionServiceImpl implements TransactionService {
 	@Autowired
 	AccountDAO accountDao;
 
+	private static Logger logger = LoggerFactory.getLogger(AccountController.class);
+
 	@Override
 	public List<Transaction> displayTransactions(String accountNumber)
 			throws Exception, AccountNumberNotFoundException {
+		logger.info("Start of DisplayTxn() Service,It input is AccountNumber '{}'", accountNumber);
 		List<Transaction> txns = new ArrayList<Transaction>();
 		Account account = accountDao.findByAccountNumberEquals(accountNumber);
 
@@ -35,6 +40,7 @@ public class TransactionServiceImpl implements TransactionService {
 			throw new AccountNumberNotFoundException("AccountNumber doesnt exist");
 
 		}
+		logger.info("End of DisplayTxn() Service,It output is TransactionList ");
 		return txns;
 	}
 
